60cd005ec4401314c6af9b13e15dad788f77288c
moaaz malek <madhalfrwat@gmail.com>
2024-09-27 16:49:53 +0300

    Update the  component

--- src/routes/edit/$editId.tsx
+++ src/routes/edit/$editId.tsx
@@ -4,6 +4,8 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { PostType } from '../../type/post'
 import { format } from 'date-fns'
 import { useUpdatePost } from '../../services/mutations'
+import { usePostsContext } from '../../context/PostsProvider'
+import { useState } from 'react'
 // import { usePostsContext } from '../../context/PostsProvider'
 
 export const Route = createFileRoute('/edit/$editId')({
@@ -11,36 +13,52 @@ export const Route = createFileRoute('/edit/$editId')({
 })
 
 function EditComponent() {
+
     const {editId}=Route.useParams()
     const navigate = useNavigate()
     const updatePost=useUpdatePost()
-    // const {posts}=usePostsContext()
-    const {register,handleSubmit}=useForm<PostType>()
+    const {posts}=usePostsContext()
+    const post=posts?.find(item => item.id ===editId)
+    const [editPost,setEditPost]=useState<{body:string,title:string}>(post?{
+      body:post.body,
+      title:post.title}:{body:" ",title:" "})
+    
     
 
-    const handleCreatePost:SubmitHandler<PostType>=(data:PostType) => {
+    const handleCreatePost=(e:React.FormEvent) => {
+      e.preventDefault()
       
       const datetime=format(new Date(),'MMMM dd, yyyy pp')
-       const newPost={id:editId,datetime:datetime,body:data.body,title:data.title}
+       const newPost={id:editId,datetime:datetime,body:editPost.body,title:editPost.title}
        console.log(newPost)
         updatePost.mutate(newPost)
        navigate({to:'/'})
 
     }
+    const handleTitleChange=(UpdatedTitle:string) =>{
+      setEditPost(prevPost => ({...prevPost,title:UpdatedTitle}))
+    }
+    const handleBodyChange=(UpdatedBody:string) =>{
+      setEditPost(prevPost => ({...prevPost,body:UpdatedBody}))
+    }
 
     return (
       <div>
-      <h1 className='pl-3 text-xl font-bold  pt-3 text-center '>New Post</h1>
-      <form  className='flex flex-col gap-5 p-5 justify-start  items-center ' onSubmit={handleSubmit(handleCreatePost)}>
+      <h1 className='pl-3 text-xl font-bold  pt-3 text-center '>Update Post</h1>
+      <form  className='flex flex-col gap-5 p-5 justify-start  items-center ' onSubmit={handleCreatePost}>
         <input type='text'
         className=' w-full md:w-2/3 border-2 border-black  border-solid  text-black pl-3 min-h-12 max-h-12 rounded-xl focus:bg-gray-100 '
-        {...register("title")}
+        value={editPost.title}
+        onChange={(e) => handleTitleChange(e.target.value)}
+        
         
 
                required
                />
                <textarea className=' w-full bg-gray-200  md:w-2/3 min-h-48 max-h-52 rounded-lg  p-3 text-lg tracking-wide'
-                 {...register("body")}
+               value={editPost.body}
+               onChange={(e) => handleBodyChange(e.target.value)}
+                 
 
                required
 
